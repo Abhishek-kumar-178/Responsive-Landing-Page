c1cea02d58142897d11a430e92ea95a1cde019dc
Abhishek Kumar <89182442+Abhishek-kumar-178@users.noreply.github.com>
2024-02-13 19:56:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#f4f4f4'; // Change background color on scroll
+        navbar.style.backgroundColor = 'green'; 
     } else {
         navbar.style.backgroundColor = 'transparent';
     }
